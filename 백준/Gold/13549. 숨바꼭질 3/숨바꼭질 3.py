f48ed300256dc777b8dccc85e1a8c471974dc8cf
Yunhui Lim <lyhhh0320@gmail.com>
2024-09-01 02:38:34 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 304 ms, Memory: 71620 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,40 @@
+import heapq
+import sys
+sys.stdin.readline
+INF = int(1e9)
+
+n, k = map(int, input().split())
+
+graph = [[] for _ in range(100001)]
+distance = [INF] * 100001
+
+graph[0].append((1, 1))
+graph[100000].append((99999, 1))
+
+for i in range(1, 100000):
+    graph[i].append((i - 1, 1))
+    graph[i].append((i + 1, 1))
+
+    if 2 * i <= 100000:
+        graph[i].append((2 * i, 0))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = i[1] + dist
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(n)
+
+print(distance[k])
