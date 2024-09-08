3a91d9ff4d4f445a9b5277164948e12d472ab7e6
Yunhui Lim <lyhhh0320@gmail.com>
2024-09-09 03:00:58 +0900

    [Gold IV] Title: 가르침, Time: 1380 ms, Memory: 112888 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.py
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -2,20 +2,10 @@ import sys
 
 n, k = map(int, input().split())
 
-# k 가 5보다 작으면 어떤 언어도 배울 수 없음
-if k < 5:
-    print(0)
-    exit()
-# k 가 26이면 모든 언어를 배울 수 있음
-elif k == 26:
-    print(n)
-    exit()
-
-answer = 0
 words = [set(sys.stdin.readline().rstrip()) for _ in range(n)]
 learn = [0] * 26
+answer = 0
 
-# 적어도 언어 하나는 배우기위해 a,c,i,n,t 는 무조건 배워야함
 for c in ('a', 'c', 'i', 'n', 't'):
     learn[ord(c) - ord('a')] = 1
 
@@ -34,7 +24,7 @@ def dfs(idx, cnt):
                 readcnt += 1
         answer = max(answer, readcnt)
         return
-
+    
     for i in range(idx, 26):
         if not learn[i]:
             learn[i] = True
