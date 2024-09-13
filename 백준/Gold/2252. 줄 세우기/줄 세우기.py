0f94dfa61e24e7ab44d377e123358716cead3333
Yunhui Lim <lyhhh0320@gmail.com>
2024-09-14 04:28:17 +0900

    [Gold III] Title: 줄 세우기, Time: 2500 ms, Memory: 40856 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -3,38 +3,32 @@ sys.stdin.readline
 
 n, m = map(int, input().split())
 
-# lst = []
-
-# for _ in range(m):
-#     a, b = map(int, input().split())
-#     if a not in lst:
-#         lst.append(a)
-#     if b not in lst:
-#         lst.append(b)
-#     if b in lst:
-
-compare = [[] for _ in range(n + 1)]
-
+# n 명의 학생들의 우선순위를 저장하기 위한 리스트
 arr = [0] * (n + 1)
+# 비교된 적 있는 학생들 체크하는 리스트
 visited = [0] * (n + 1)
+# 특정 학생에 대해 자기보다 큰 학생들 저장하는 리스트
+# 만약 compare[1]의 값이 [2,3]이라면 1번 학생보다 큰 학생들은 2번,3번 학생이라는 뜻
+compare = [[] for _ in range(n + 1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
+    # a보다 큰 학생 저장
     compare[a].append(b)
-    if arr[a] >= arr[b]:
-        arr[b] = arr[a] + 1
+    # 비교된 학생들 체크
     visited[a] = 1
     visited[b] = 1
-    if visited[b]:
-        for higher in compare[b]:
-            if higher != a:
-                arr[higher] += 1
+    # a학생의 우선순위가 b학생보다 크거나 같은 경우에만 우선순위 재정렬 필요
+    if arr[a] >= arr[b]:
+        arr[b] = arr[a] + 1
+        
+        if visited[b]:
+            for higher in compare[b]:
+                if higher != a:
+                    arr[higher] += 1
 
 for idx, val in enumerate(arr):
     arr[idx] = (val, idx)
-
-# print(arr)
-
 arr.sort()
 
 for i in range(1, len(arr)):
