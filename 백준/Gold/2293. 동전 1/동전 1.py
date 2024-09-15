25e7b645d82887891ddedc3ebb2a428b9342198b
Yunhui Lim <lyhhh0320@gmail.com>
2024-09-16 07:19:37 +0900

    [Gold V] Title: 동전 1, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = []
+dp = [0] * (k + 1)
+
+for _ in range(n):
+    coins.append(int(input()))
+
+dp[0] = 1
+
+for coin in coins:
+    for i in range(coin, k + 1):
+        dp[i] += dp[i - coin]
+
+       
+print(dp[k])
