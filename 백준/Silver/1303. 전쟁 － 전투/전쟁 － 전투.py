85ed0b398965be128a11ab9e778a044bbc8fed58
Yunhui Lim <lyhhh0320@gmail.com>
2024-09-21 01:05:04 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 44 ms, Memory: 31132 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -1,47 +1,33 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(3000000)
 
 n, m = map(int, input().split())
-graph = [list(input()) for _ in range(m)]
 
-visited = [[False] * n for _ in range(m)]
+board = [list(input()) for _ in range(m)]
 
-dxs = [-1, 1, 0, 0]
-dys = [0, 0, -1, 1]
+def dfs(x, y, cnt):
+    color = board[x][y]
+    board[x][y] = 1
 
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
-def bfs(i, j, color):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-    cnt = 0
-    queue = deque()
+        if 0 <= nx < m and 0 <= ny < n and board[nx][ny] == color:
+            cnt = dfs(nx, ny, cnt + 1)
+ 
+    return cnt
+    
+cntW, cntB = 0, 0
 
-    if graph[i][j] == color and not visited[i][j]:
-        queue.append((i, j))
-        visited[i][j] = True
-        cnt += 1
-
-
-    while queue:
-        x, y = queue.popleft()
-        for dir in range(len(dxs)):
-            nx = x + dxs[dir]
-            ny = y + dys[dir]
-
-            if nx < 0 or nx >= m or ny < 0 or ny >= n:
-                continue
-
-            if graph[nx][ny] == color and not visited[nx][ny]:
-                queue.append((nx, ny))
-                visited[nx][ny] = True
-                cnt += 1
-
-    return cnt*cnt
-
-w, b = 0, 0
 for i in range(m):
     for j in range(n):
-        if graph[i][j] == 'W':
-            w += bfs(i, j, graph[i][j])
-        elif graph[i][j] == 'B':
-            b += bfs(i, j, graph[i][j])
+        if board[i][j] == 'W':
+            cntW += (dfs(i, j, 1))**2
+        elif board[i][j] == 'B':
+            cntB += (dfs(i, j, 1))**2
 
-print(w, b)
+print(cntW, cntB)
