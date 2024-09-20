d242ae357ee00b5789b16a5bbe56622d20dd3d5b
Yunhui Lim <lyhhh0320@gmail.com>
2024-09-20 23:48:50 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,44 +1,40 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-n, m, v = map(int, input().split())
-
+n, m, v = map(int, input().rstrip().split())
 graph = [[] for _ in range(n + 1)]
 
 for _ in range(m):
-    u, w = map(int, input().split())
-    graph[u].append(w)
-    graph[w].append(u)
+    a, b = map(int, input().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-for neighbors in graph:
-    neighbors.sort()
+for linked in graph:
+    linked.sort()
 
-visited = [False] * (n + 1)
+visited = [0] * (n + 1)
 
-def dfs(v):
-    visited[v] = True
-    print(v, end=' ')
-    for neighbor in graph[v]:
-        if not visited[neighbor]:
-            dfs(neighbor)
+def dfs(start):
+    visited[start] = 1
+    print(start, end=" ")
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(i)
 
-def bfs(v):
-    queue = deque([v])
-    visited[v] = True
 
-    while queue:
-        val = queue.popleft()
-        print(val, end=' ')
-
-        for neighbor in graph[val]:
-            if not visited[neighbor]:
-                queue.append(neighbor)
-                visited[neighbor] = True
-                
-
+def bfs(start):
+    q = deque([start])
+    visited[start] = 1
+    while q:
+        v = q.popleft()
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[i]:
+                q.append(i)
+                visited[i] = 1
 
 dfs(v)
 print()
-
-visited = [False] * (n + 1)
-
+visited = [0] * (n + 1)
 bfs(v)
