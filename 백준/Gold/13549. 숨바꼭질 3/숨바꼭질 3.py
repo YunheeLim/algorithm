c39a485e6150a1f06948d37c92350a90287735c5
Yunhui Lim <lyhhh0320@gmail.com>
2024-10-01 11:02:17 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 80 ms, Memory: 35336 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,40 +1,42 @@
-import heapq
-import sys
-sys.stdin.readline
-INF = int(1e9)
+from collections import deque
+
+MAX = 100001
 
 n, k = map(int, input().split())
+time = [ 0 ] * (MAX)
+q = deque([n])
 
-graph = [[] for _ in range(100001)]
-distance = [INF] * 100001
-
-graph[0].append((1, 1))
-graph[100000].append((99999, 1))
-
-for i in range(1, 100000):
-    graph[i].append((i - 1, 1))
-    graph[i].append((i + 1, 1))
-
-    if 2 * i <= 100000:
-        graph[i].append((2 * i, 0))
-
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
+# 너비 우선 탐색
+def bfs(cur, k):
+    q = deque()
+    if cur == 0:
+        q.append(1)
+    else:
+        q.append(cur)
 
     while q:
-        dist, now = heapq.heappop(q)
+        cur = q.popleft()
 
-        if distance[now] < dist:
-            continue
+        # 동생을 찾았을 때
+        if cur == k:
+            return time[cur]
+            
+        # 3가지 방법 탐색
+        for i in [cur * 2, cur - 1, cur + 1]:
+            # 범위 안에 있고 방문하지 않았거나 다음 방문이 현재 + 1일 때
+            if 0 <= i < MAX and time[i] == 0:
+                if i == cur * 2:
+                    time[i] = time[cur]
+                    q.appendleft(i)
+                else:
+                    time[i] = time[cur] + 1
+                    q.append(i)
 
-        for i in graph[now]:
-            cost = i[1] + dist
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-
-dijkstra(n)
-
-print(distance[k])
+# 결과 출력
+if n == 0:
+    if k == 0:
+        print(0)
+    else:
+        print(bfs(n, k) + 1)
+else:
+    print(bfs(n, k))
