d24e93eeda7f57186d548c38f4a95abe63ae8fe0
Yunhui Lim <lyhhh0320@gmail.com>
2024-10-03 09:42:15 +0900

    [Silver V] Title: 올림픽, Time: 60 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,70 +1,34 @@
 import sys
-input = sys.stdin.readline
+sys.stdin.readline
 
 n, k = map(int, input().split())
-arr = []
-
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
-
+arr = [list(map(int, input().split())) for _ in range(n)]
 rank = []
-rank.append(arr[0])
 
-cnt = 1
+for i in range(n):
+    rank.append((arr[i][1] * 1000000**2 + arr[i][2] * 1000000 + arr[i][3], arr[i][0]))
+
+rank.sort(reverse=True)
+
+ans = 0
+draw = 0
+# print('[rank]', rank)
+
+if rank[0][1] == k:
+    print(1)
+    exit()
 
 for i in range(1, n):
-    idx = 1
-    draw = False
-
-    for j in range(cnt):
-        compare = 0
-
-        if type(rank[j][0]) == list:
-            compare = rank[j][1]
-        else:
-            compare = rank[j]
-            if compare[0] == 0:
-                continue
-
-        if arr[i][1] > compare[1]:
-            idx = j
-        elif arr[i][1] == compare[1]:
-            if arr[i][2] > compare[2]:
-                idx = j
-            elif arr[i][2] == compare[2]:
-                if arr[i][3] > compare[3]:
-                    idx = j
-                elif arr[i][3] == compare[3]:
-                    draw = True
-                    idx = j
-                    break
-                else:
-                    idx = j + 1
-            else:
-                idx = j + 1
-        else:
-            idx = j + 1
-
-    if draw:
-        temp = rank[idx]
-        rank.insert(idx, [arr[i], temp])
-  
-        rank[idx + 1] = [0, -1, -1, -1]
-        cnt += 1
-
+    if rank[i][0] == rank[i-1][0]:
+        draw += 1
     else:
-        rank.insert(idx, arr[i])
-        cnt += 1
+        draw = 0
 
-#print('[rank]', rank)
-for i in range(len(rank)):
-    # print(rank[i])
-    if type(rank[i][0]) == list:
-        for j in range(len(rank[i])):
-            if k == rank[i][j][0]:
-                print(i+1)
-                exit()
-    else:
-        if k == rank[i][0]:
-            print(i+1)
-            exit()
+    if rank[i][1] == k:
+        if draw:
+            ans = i - draw
+        else:
+            ans = i
+        break
+
+print(ans + 1)
