203742e41caa646b15089dda4388c4e7b65a57bc
Yunhui Lim <lyhhh0320@gmail.com>
2024-10-09 05:32:26 +0900

    [level 2] Title: 요격 시스템, Time: 921.66 ms, Memory: 94.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool cmp(vector<int> v1, vector<int> v2) {
+    if (v1[1] < v2[1]) {
+        return true;
+    } else if (v1[1] == v2[1]) {
+        if (v1[0] <= v2[0]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+int solution(vector<vector<int>> targets) {
+    int answer = 0;
+    
+    sort(targets.begin(), targets.end(), cmp);
+        
+    int check = -1;
+    
+    for (int i = 0; i < targets.size(); i++) {
+        if (targets[i][0] >= check) {
+            answer++;
+            check = targets[i][1];
+        }
+    }
+    
+    return answer;
+}
