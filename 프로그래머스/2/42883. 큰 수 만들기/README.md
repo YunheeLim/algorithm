012bba0409920e9e192f045445f8324727280c2d
Yunhui Lim <lyhhh0320@gmail.com>
2024-10-13 01:04:46 +0900

    [level 2] Title: 큰 수 만들기, Time: 4.39 ms, Memory: 5.79 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
 
 ### 성능 요약
 
-메모리: 5.74 MB, 시간: 4.36 ms
+메모리: 5.79 MB, 시간: 4.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 09일 07:34:34
+2024년 10월 13일 01:04:45
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -8,17 +8,18 @@ vector<char> stack;
 string solution(string number, int k) {
     string answer = "";
     
-    for (int i = 0; i < number.size(); i++) {
+    for (int i = 0; i < number.length(); i++) {
         while (stack.size() && stack[stack.size() - 1] < number[i] && k) {
             stack.pop_back();
             k--;
         }
-        
         stack.push_back(number[i]);
     }
     
-    if (k != 0) {
-        stack.pop_back();
+    if (k) {
+        while (k--) {
+            stack.pop_back();
+        }
     }
     
     for (auto elem : stack) {
