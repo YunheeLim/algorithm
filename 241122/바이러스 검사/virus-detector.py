c410baed302fbf2ed5ca754c667a84bc6508319e
Yunhui Lim <lyhhh0320@gmail.com>
2024-11-22 04:40:17 +0900

    Title: 바이러스 검사, Time: 38ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241122/바이러스 검사/virus-detector.py
+++ 241122/바이러스 검사/virus-detector.py
@@ -11,7 +11,10 @@ for i in c:
     i -= p[0]
     if i:
         num = i // p[1]
-        remain = i % p[1] * num
-        ans = max(ans, num + remain + 1)
+        remain = i % p[1]
+        if remain:
+            ans = max(ans, n * (num + 2))
+        else:
+            ans = max(ans, n * (num + 1))
 
-print(ans * n)
+print(ans)
