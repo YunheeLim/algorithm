90233ac93597c09688134a3affe06827c64226b9
Yunhui Lim <lyhhh0320@gmail.com>
2024-11-22 04:27:05 +0900

    Title: 바이러스 검사, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241122/바이러스 검사/virus-detector.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+c = list(map(int, input().rstrip().split()))
+p = list(map(int, input().rstrip().split()))
+
+ans = 1
+
+for i in c:
+    i -= p[0]
+    if i:
+        num = i // p[1]
+        remain = i % p[1] * num
+        ans = max(ans, num + remain + 1)
+
+print(ans)
