d76ae1735dd6353085241c23023581d6b79fdb27
Yunhui Lim <lyhhh0320@gmail.com>
2024-11-23 02:58:21 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 296 ms, Memory: 34136 KB -BaekjoonHub

---
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -0,0 +1,60 @@
+from collections import deque
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+moves = [list(map(int, input().split())) for _ in range(m)]
+
+# 초기 구름 위치 설정
+clouds = deque([(n - 2, 0), (n - 2, 1), (n - 1, 0), (n - 1, 1)])
+
+# 이동 방향
+dx = [0, -1, -1, -1, 0, 1, 1, 1]
+dy = [-1, -1, 0, 1, 1, 1, 0, -1]
+
+# 물복사 대각선 방향
+diag_dx = [-1, -1, 1, 1]
+diag_dy = [-1, 1, -1, 1]
+
+# 물복사 버그 해결
+def copy(x, y):
+    for i in range(4):  # 대각선 방향
+        nx, ny = x + diag_dx[i], y + diag_dy[i]
+        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] > 0:
+            arr[x][y] += 1
+
+# 새로운 구름 생성
+def newCloud(visited):
+    new_clouds = deque()
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] >= 2 and not visited[i][j]:
+                new_clouds.append((i, j))
+                arr[i][j] -= 2
+    return new_clouds
+
+# 구름 이동
+for move in moves:
+    d, s = move
+    d -= 1  # 방향 조정
+    visited = [[False] * n for _ in range(n)]
+
+    # 구름 이동 및 물 증가
+    for _ in range(len(clouds)):
+        x, y = clouds.popleft()
+        nx = (x + dx[d] * s) % n  # 경계 처리
+        ny = (y + dy[d] * s) % n
+        arr[nx][ny] += 1
+        visited[nx][ny] = True
+        clouds.append((nx, ny))
+
+    # 물복사
+    while clouds:
+        x, y = clouds.popleft()
+        copy(x, y)
+
+    # 새 구름 생성
+    clouds = newCloud(visited)
+
+# 결과 계산
+ans = sum(sum(row) for row in arr)
+print(ans)
