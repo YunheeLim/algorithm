07d4efc02b128c7c0cdc5f886cf395c952cb0baa
Yunhui Lim <lyhhh0320@gmail.com>
2025-01-23 22:16:16 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 673.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -6,7 +6,7 @@ def solution(alp, cop, problems):
         alp = max_alp
     if max_cop < cop:
         cop = max_cop
-        
+    
     dp = [[151 for i in range(max_cop + 1)] for j in range(max_alp + 1)]
     dp[alp][cop] = 0
     
@@ -17,12 +17,12 @@ def solution(alp, cop, problems):
             
             if j + 1 <= max_cop:
                 dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1)
-            
+                
             for p in problems:
                 alp_req, cop_req, alp_rwd, cop_rwd, cost = map(int, p)
                 if alp_req <= i and cop_req <= j:
                     next_alp = min(max_alp, i + alp_rwd)
                     next_cop = min(max_cop, j + cop_rwd)
                     dp[next_alp][next_cop] = min(dp[next_alp][next_cop], dp[i][j] + cost)
-                    
+    
     return dp[-1][-1]
