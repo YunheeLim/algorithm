24c3ed609e2bcbd927c50862a3b04a219853a9fd
Yunhui Lim <lyhhh0320@gmail.com>
2025-01-26 19:10:23 +0900

    [level 3] Title: 양과 늑대, Time: 11.39 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -0,0 +1,20 @@
+def solution(info, edges):
+    answer = []
+    visited = [False] * len(info)
+    
+    def dfs(sheeps, wolves):
+        if sheeps > wolves:
+            answer.append(sheeps)
+        else:
+            return
+        for p, c in edges:
+            if visited[p] and not visited[c]:
+                visited[c] = True
+                if info[c]:
+                    dfs(sheeps, wolves + 1)
+                else:
+                    dfs(sheeps + 1, wolves)
+                visited[c] = False
+    visited[0] = True
+    dfs(1, 0)
+    return max(answer)
