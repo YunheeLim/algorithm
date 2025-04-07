3818b7386dfa78b28fcf0643e2ada601990341d4
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-07 21:17:29 +0900

    Title: 고대 문명 유적 탐사, Time: 144ms, Memory: 21MB, Status: Passed - Codetree

--- 250407/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 250407/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 k, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(5)]
 nums = list(map(int, input().split()))
@@ -51,60 +49,13 @@ def get_max_val(grid):
                         grid[x][y] = -1
     return max_val, total_route
 
-# # 벽면의 숫자가 채워지기 시작하는 지점 찾는 함수 (한 덩어리에 대해서만)
-# def find_start(grid, x, y, visited, sx, sy):
-#     visited[x][y] = True
-#     for i in range(4):
-#         nx = x + dx[i]
-#         ny = y + dy[i]
-#         if 0 <= nx < 5 and 0 <= ny < 5:
-#             if grid[nx][ny] == grid[x][y] and not visited[nx][ny]:
-#                 sx, sy = find_start(grid, nx, ny, visited, sx, sy)
-#     return (x, y) if (y < sy or (y == sy and x > sx)) else (sx, sy)
-
-# # 벽면의 숫자가 채워지기 시작하는 지점들 찾는 함수 (여러 덩어리에 대해서)
-# def find_start_points(grid):
-#     start_points = []
-#     visited2 = [[False] * 5 for _ in range(5)]
-#     for i in range(5):
-#         for j in range(5):
-#             # 사라진 덩어리가 여러개 있을 수 있기 때문에 시작지점들 배열로 저장
-#             if grid[i][j] == -1 and not visited2[i][j]:
-#                 start_x, start_y = find_start(grid, i, j, visited2, 4, 4)
-#                 start_points.append((start_x, start_y))
-#     # 열이 가장 작고 행이 가장 큰 곳 순으로 정렬
-#     start_points.sort(key=lambda x: (-x[0], x[1]))
-#     return start_points
-
-# 유물 사라진 곳 숫자 채우기(아래 -> 위, 왼 -> 오 순서)
+# 유물 사라진 곳 숫자 채우기
 def fill(grid, route, idx):
-    route.sort(key=lambda x: (x[1], -x[0]))
+    route.sort(key=lambda x: (x[1], -x[0])) # 작은 열, 큰 행 순으로 정렬
     for i, j in route:
         grid[i][j] = nums[idx]
         idx += 1
-    # 두 덩어리가 합쳐졌고, 이미 한 덩어리는 탐색한 경우
-    # if grid[x][y] > 0:
-    #     return idx
-    
-    # # -2로 그룹 넘버링
-    # q = deque([(x, y)])
-    # grid[x][y] = -2
-    # while q:
-    #     cx, cy = q.popleft()
-    #     for i in range(4):
-    #         nx = cx + dx[i]
-    #         ny = cy + dy[i]
-    #         if 0 <= nx < 5 and 0 <= ny < 5 and grid[nx][ny] == -1:
-    #             grid[nx][ny] = -2
-    #             q.append((nx, ny))
-    
-    # for j in range(y, 5):
-    #     for i in range(4, -1, -1):  
-    #         if grid[i][j] == -2:
-    #             grid[i][j] = nums[idx]
-    #             idx += 1
-
-    return idx
+    return idx # 마지막 조각까지 채우고 그 다음 벽면 숫자의 인덱스 반환
 
 flag = True # 탐사 진행 가능 여부
 wall_idx = 0 # 현재 가리키고 있는 벽면 숫자의 인덱스
@@ -112,6 +63,7 @@ turn = 0
 while turn < k and flag:
     flag = True
     answer = 0 # 각 턴마다의 유물 가치
+    
     # ==== 탐사 진행 시작 ====
     # 중심점 지정
     candidate = [] # 유물 가치 최대가 되는 배열 후보(유물가치, 배열, 회전각도, 회전중심열, 회전중심행)
@@ -126,8 +78,6 @@ while turn < k and flag:
                     row.append(arr[i][j])
                 tmp.append(row)
 
-            # print('중심점', r, c)
-
             # 3방향 회전
             rotated_tmp = []
             for degree in range(1, 4):
@@ -137,11 +87,6 @@ while turn < k and flag:
                 for rr in range(r, r + 3):
                     for cc in range(c, c + 3):
                         copy_arr[rr][cc] = rotated_tmp[rr - r][cc - c]
-                # 변경 적용한 배열 확인
-                # print('회전각도:', degree * 90)
-                # for v in copy_arr:
-                #     print(v)
-                # print('유물 가치', get_max_val(copy_arr))
                 get_max_value, total_route = get_max_val(copy_arr)
                 if get_max_value >= max_value:
                     candidate.append([get_max_value, copy_arr, degree, c, r, total_route])
@@ -153,35 +98,15 @@ while turn < k and flag:
         flag = False
         break
     selected_arr = candidate[0][1] # 유물가치가 최대가 되도록 회전한 배열
-    # print(candidate[0])
-    # if turn == 3:
-    #     break
-    #선택된 배열 확인
-    # print("===selected===")
-    # for e in selected_arr:
-    #     print(e) 
-    # print("======")
     # ==== 탐사 진행 끝 ====
 
     # ==== 유물 1차 획득 시작 ====
-    removed = candidate[0][0] # 사라진 조각 개수
+    answer += candidate[0][0] # 사라진 조각 개수
     total_route = candidate[0][5] # 사라진 조각들의 좌표
-    answer += removed
-
-    # start_points = find_start_points(selected_arr)
-    # start_points.sort(key=lambda x: (x[1], -x[0]))
-    # print('===시작지점들===')
-    # for x, y in start_points:
-    #     print(x, y)
-    # print('======')
 
     wall_idx = fill(selected_arr, total_route, wall_idx) # 다 채우고 다음 진행을 위해 벽면 인덱스 갱신
-
-    # print("===filled(1차)===")
-    # for e in selected_arr:
-    #     print(e)
-    # print("======")
     # ==== 유물 1차 획득 끝====
+
     # ==== 유물 연쇄 획득 시작====
     flag2 = True
     while flag2:
@@ -190,28 +115,13 @@ while turn < k and flag:
             flag2 = False
             break
         answer += max_value
-        # start_points = find_start_points(selected_arr)
-        # start_points.sort(key=lambda x: (x[1], -x[0]))
-        # print("===removed(연쇄)===")
-        # for e in selected_arr:
-        #     print(e)
-        # print("======")
-        # print(start_points)
 
         wall_idx = fill(selected_arr, total_route, wall_idx) # 다 채우고 다음 진행을 위해 벽면 인덱스 갱신
-        
-        # print("===filled(연쇄)===")
-        # for e in selected_arr:
-        #     print(e)
-        # print("======")
     # ==== 유물 연쇄 획득 끝====
+
+    # 정답 출력
     print(answer, end=" ")
+
     # 다음 진행을 위해 arr 갱신
     arr = [row[:] for row in selected_arr]
     turn += 1
-    # print("wall idx:", wall_idx)
-
-    # print('==턴 마지막 결과==')
-    # for e in arr:
-    #     print(e)
-    # print('=====')
