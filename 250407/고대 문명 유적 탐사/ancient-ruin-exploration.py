2631d296b9fed4c22090c4650b368485a983c109
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-07 17:58:30 +0900

    Title: 고대 문명 유적 탐사, Time: 141ms, Memory: 21MB, Status: Runtime Error - Codetree

--- 250407/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 250407/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -22,16 +22,15 @@ def rotate(grid, degree):
 # 연결 요소 개수 세기
 def dfs(grid, x, y, visited, path):
     visited[x][y] = True
-    path.append((x, y))
+    path.add((x, y))
     cnt = 1
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < 5 and 0 <= ny < 5:
             if grid[nx][ny] == grid[x][y] and not visited[nx][ny]:
-                c, p = dfs(grid, nx, ny, visited, path)
-                cnt += c
-    return cnt, path
+                cnt += dfs(grid, nx, ny, visited, path)
+    return cnt
 
 # 유물 가치 계산
 def get_max_val(grid):
@@ -41,9 +40,8 @@ def get_max_val(grid):
         for j in range(5):
             if not visited[i][j]:
                 # 유물 삭제를 위해 기록 저장
-                path = []
-                # print('위치:', i, j, "값:", grid[i][j], "연결조각개수:", dfs(grid, i, j, visited))
-                val, result_path = dfs(grid, i, j, visited, path)
+                path = set()
+                val = dfs(grid, i, j, visited, path)
                 if val >= 3: # 유물 완성
                     max_val += val
                     # 유물 삭제
@@ -52,28 +50,15 @@ def get_max_val(grid):
     return max_val
 
 # 벽면의 숫자가 채워지기 시작하는 지점 찾는 함수 (한 덩어리에 대해서만)
-def find_start(grid, x, y, visited):
-    q = deque()
-    q.append((x, y))
+def find_start(grid, x, y, visited, sx, sy):
     visited[x][y] = True
-    min_x, min_y = x, y  # 시작 지점 초기화
-
-    while q:
-        cx, cy = q.popleft()
-
-        # 좌상단 우선 기준 갱신
-        if cy < min_y or (cy == min_y and cx > min_x):
-            min_x, min_y = cx, cy
-
-        for i in range(4):
-            nx = cx + dx[i]
-            ny = cy + dy[i]
-            if 0 <= nx < 5 and 0 <= ny < 5:
-                if grid[nx][ny] == grid[cx][cy] and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    q.append((nx, ny))
-
-    return (min_x, min_y)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < 5 and 0 <= ny < 5:
+            if grid[nx][ny] == grid[x][y] and not visited[nx][ny]:
+                sx, sy = find_start(grid, nx, ny, visited, sx, sy)
+    return (x, y) if (y < sy or (y == sy and x > sx)) else (sx, sy)
 
 # 벽면의 숫자가 채워지기 시작하는 지점들 찾는 함수 (여러 덩어리에 대해서)
 def find_start_points(grid):
@@ -82,28 +67,33 @@ def find_start_points(grid):
     for i in range(5):
         for j in range(5):
             # 사라진 덩어리가 여러개 있을 수 있기 때문에 시작지점들 배열로 저장
-            if selected_arr[i][j] == -1 and not visited2[i][j]:
-                start_x, start_y = find_start(selected_arr, i, j, visited2)
+            if grid[i][j] == -1 and not visited2[i][j]:
+                start_x, start_y = find_start(grid, i, j, visited2, 4, 4)
                 start_points.append((start_x, start_y))
     # 열이 가장 작고 행이 가장 큰 곳 순으로 정렬
     start_points.sort(key=lambda x: (-x[0], x[1]))
     return start_points
 
-# 벽면의 숫자 채우기 (북-남-동 순서 보장 위해 bfs로 진행)
+# 벽면의 숫자 채우기(아래 -> 위, 왼 -> 오 순서)
 def fill(grid, x, y, idx):
+    # -2로 그룹 넘버링
     q = deque([(x, y)])
-    grid[x][y] = nums[idx]
-    idx += 1
+    grid[x][y] = -2
     while q:
-        x, y = q.popleft()
+        cx, cy = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = cx + dx[i]
+            ny = cy + dy[i]
             if 0 <= nx < 5 and 0 <= ny < 5 and grid[nx][ny] == -1:
-                grid[nx][ny] = nums[idx]
-                if idx < len(nums) - 1:
-                    idx += 1
+                grid[nx][ny] = -2
                 q.append((nx, ny))
+    
+    for j in range(y, 5):
+        for i in range(4, -1, -1):  
+            if grid[i][j] == -2:
+                grid[i][j] = nums[idx]
+                idx += 1
+
     return idx
 
 flag = True # 탐사 진행 가능 여부
@@ -154,12 +144,10 @@ while turn < k and flag:
         break
     selected_arr = candidate[0][1] # 유물가치가 최대가 되도록 회전한 배열
     #선택된 배열 확인
-    # if turn == 2:
     # print("===selected===")
     # for e in selected_arr:
     #     print(e) 
     # print("======")
-        # break
     # ==== 탐사 진행 끝 ====
 
     # ==== 유물 1차 획득 시작 ====
@@ -191,11 +179,11 @@ while turn < k and flag:
             break
         answer += max_value
         start_points = find_start_points(selected_arr)
-            # print("===removed(연쇄)===")
-            # for e in selected_arr:
-            #     print(e)
-            # print("======")
-            # print(start_points)
+        # print("===removed(연쇄)===")
+        # for e in selected_arr:
+        #     print(e)
+        # print("======")
+        # print(start_points)
 
         for start_x, start_y in start_points:
             wall_idx = fill(selected_arr, start_x, start_y, wall_idx) # 다 채우고 다음 진행을 위해 벽면 인덱스 갱신
@@ -209,3 +197,7 @@ while turn < k and flag:
     # 다음 진행을 위해 arr 갱신
     arr = [row[:] for row in selected_arr]
     turn += 1
+    # print('==턴 마지막 결과==')
+    # for e in arr:
+    #     print(e)
+    # print('=====')
