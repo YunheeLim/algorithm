cd6e3d5780a21925cdb0e658c35f27d8b0741c55
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-11 14:23:43 +0900

    Title: 메이즈 러너, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250411/메이즈 러너/maze-runner.py
+++ 250411/메이즈 러너/maze-runner.py
@@ -33,27 +33,28 @@ def move(x, y, ex, ey):
     return x, y, get_min_dist(x, y, ex, ey)
 
 def make_rectangle(dist, sx, sy, ex, ey):
-    side = dist + 1
-    candidates = []
+    dist += 1
+    for sz in range(2, dist + 1):  # 정사각형 한 변 크기
+        for x1 in range(n - sz + 1):
+            for y1 in range(n - sz + 1):
+                x2, y2 = x1 + sz - 1, y1 + sz - 1
+                
+                if not (x1 <= ex <= x2 and y1 <= ey <= y2):
+                    continue
 
-    # (r1, c1) = 정사각형의 좌측 상단
-    for r1 in range(n - side + 1):
-        for c1 in range(n - side + 1):
-            r2 = r1 + side - 1
-            c2 = c1 + side - 1
+                is_traveler_in = False
+                for l in range(m):  # traveler[l] = (x, y)
+                    tx, ty = people_pos[l][0], people_pos[l][1]
+                    if x1 <= tx <= x2 and y1 <= ty <= y2:
+                        if not (tx == ex and ty == ey):
+                            is_traveler_in = True
+                            break
 
-            # sx, sy 와 ex, ey 가 모두 포함되어야 함
-            if (r1 <= sx <= r2 and c1 <= sy <= c2 and
-                r1 <= ex <= r2 and c1 <= ey <= c2):
-                candidates.append((r1, c1, r2, c2))
+                if is_traveler_in:
+                    sx = x1
+                    sy = y1
+                    return sz, sx, sy, sx + sz - 1, sy + sz - 1
 
-    if not candidates:
-        return False  # 만들 수 있는 정사각형 없음
-
-    # 조건: r이 가장 작고, 같다면 c가 가장 작은 것
-    candidates.sort()
-    r1, c1, r2, c2 = candidates[0]
-    return side, r1, c1, r2, c2
 
 def rotate(side, sx, sy, endx, endy):
     global ex, ey
@@ -128,12 +129,14 @@ for turn in range(k):
             people_cnt -= 1
         # else:
         #     updated_people_pos.append([people_pos[idx][0], people_pos[idx][1], people_pos[idx][2], people_pos[idx][3]])
-
     people_arr = [row[:] for row in updated_people_arr]
     # print("===사람들 이동===")
     # for e in people_arr:
     #     print(e)
-    # print('kajhf', people_pos)
+
+    # 남아있는 사람 없음
+    if people_cnt == 0:
+        break
 
     # 출구와 가까운 순으로 정렬
     tmp = [row[:] for row in people_pos]
@@ -143,11 +146,14 @@ for turn in range(k):
     candidate_rect = []
     for candi in candidate_people:
         idx = candi[3]
-        if make_rectangle(people_pos[idx][2], people_pos[idx][0], people_pos[idx][1], ex, ey) != False: # 사각형이 범위 넘어가지 않을때
-            candidate_rect.append(make_rectangle(people_pos[idx][2], people_pos[idx][0], people_pos[idx][1], ex, ey))
+        rect = make_rectangle(people_pos[idx][2], people_pos[idx][0], people_pos[idx][1], ex, ey)
+        if rect != False: # 사각형이 범위 넘어가지 않을때
+            candidate_rect.append(rect)
+    # print(candidate_rect)
     candidate_rect.sort()
     selected_rect_start = candidate_rect[0]
-    ex, ey = rotate(selected_rect_start[0], selected_rect_start[1], selected_rect_start[2], selected_rect_start[3], selected_rect_start[4])
+    ex, ey = rotate(selected_rect_start[0], selected_rect_start[1], selected_rect_start[2], selected_rect_start
+                    , selected_rect_start[4])
     # print('selected start:', selected_rect_start)
     
     # print("===rotate arr===")
@@ -157,6 +163,6 @@ for turn in range(k):
     # for e in people_arr:
     #     print(e)
     # print('업데이트 된 출구:', ex, ey)
-
+    
 print(answer)
 print(ex + 1, ey + 1)
