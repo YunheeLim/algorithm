684705e2e0f03c022084037a5b52ca3d5f8cdbf6
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-14 23:59:00 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.31 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,28 +1,40 @@
+def first(x, sy, ey, answer):
+    global current
+    for j in range(sy, ey + 1):
+        answer[x][j] = current
+        current += 1
+
+def second(y, sx, ex, answer):
+    global current
+    for i in range(sx, ex + 1):
+        answer[i][y] = current
+        current += 1
+        
+def third(x, sy, ey, answer):
+    global current
+    for j in range(ey, sy - 1, -1):
+        answer[x][j] = current
+        current += 1
+            
+def fourth(y, sx, ex, answer):
+    global current
+    for i in range(ex, sx - 1, -1):
+        answer[i][y] = current
+        current += 1
+        
+current = 1
+
 def solution(n):
     answer = [[0] * n for _ in range(n)]
     cycle = 0
-    cnt = 1
-    while(cnt <= n*n):
-        for i in range(n):
-            if not answer[cycle][i]:
-                answer[cycle][i] = cnt
-                cnt += 1
 
-        for i in range(n):
-            if not answer[i][n-cycle-1]:
-                answer[i][n-cycle-1] = cnt
-                cnt += 1
-
-        for i in range(n):
-            if not answer[n-cycle-1][n-i-1]:
-                answer[n-cycle-1][n-i-1] = cnt
-                cnt += 1
-
-        for i in range(n):
-            if not answer[n-i-1][cycle]:
-                answer[n-i-1][cycle] = cnt
-                cnt += 1
+    while current <= n * n:
+        first(cycle, cycle, n - 1 - cycle, answer)
+        second(n - 1 - cycle, cycle + 1, n - 1 - cycle, answer)
+        third(n - 1 - cycle, cycle, n - 2 - cycle, answer)
+        fourth(cycle, cycle + 1, n - 2 - cycle, answer)
         cycle += 1
-            
-    print(answer)
+    
+    for e in answer:
+        print(e)
     return answer
