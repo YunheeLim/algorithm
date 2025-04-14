e509f6828ab7a14c06470b2a674c61d173a7bd6a
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-14 19:14:39 +0900

    [level 2] Title: 피로도, Time: 128.45 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 77.06 ms
+메모리: 14.6 MB, 시간: 128.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 18일 22:04:25
+2025년 04월 14일 19:14:37
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,19 +1,34 @@
-answer = -1
+# 가능한 던전 조합 생성
+def dfs(dungeons, path, visited):
+    if len(path) == len(dungeons):
+        # print(path)
+        combi.append(path)
+        return
+    for idx in range(len(dungeons)):
+        if visited[idx] == False:
+            visited[idx] = True
+            dfs(dungeons, path + [dungeons[idx]], visited)
+            visited[idx] = False # 백트래킹
 
-def dfs(k, arr, visited, cnt):
-    global answer
-    if answer < cnt:
-        answer = cnt
-    for i in range(len(arr)):
-        if(k >= arr[i][0] and not visited[i]):
-            visited[i] = True
-            dfs(k-arr[i][1], arr, visited, cnt + 1)
-            visited[i] = False # 백트래킹
+def max_dungeons(k, dungeon):
+    cnt = 0
+    for need, use in dungeon:
+        if k < need:
+            break
+        cnt += 1
+        k -= use
+    return cnt
     
+combi = [] # 던전 조합들
 
 def solution(k, dungeons):
-    visited = [ False ] * len(dungeons)
+    answer = 0
+    visited = [False] * len(dungeons)
+    dfs(dungeons, [], visited)
+    for c in combi:
+        max_cnt = max_dungeons(k, c)
+        # print(c, max_dungeons(k, c))
+        if max_dungeons(k, c) > answer:
+            answer = max_cnt
     
-    dfs(k, dungeons, visited, 0)
-            
     return answer
