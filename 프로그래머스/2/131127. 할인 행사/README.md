4f7442668c5a1a685787027ccf041fff34f3efa0
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-14 20:07:16 +0900

    [level 2] Title: 할인 행사, Time: 182.52 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 17.2 MB, 시간: 282.73 ms
+메모리: 16.5 MB, 시간: 182.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 20일 04:30:15
+2025년 04월 14일 20:07:14
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,20 +1,27 @@
+import copy
+
 def solution(want, number, discount):
     answer = 0
-    start = 0
-
-    while (start + 10 <= len(discount)):
-        day = 0
-        number_copy = number.copy()
-        for i in range(start, start + 10):                
-            day += 1
-
-            if discount[i] in want:
-                if number_copy[want.index(discount[i])]:
-                    number_copy[want.index(discount[i])] -= 1
-                    
-        if (sum(number_copy) == 0):
+    # 구매 품목 딕셔너리 생성
+    dic = {}
+    for goods, num in zip(want, number):
+        dic[goods] = num
+        
+    start_idx = 0
+    while start_idx + 10 <= len(discount):
+        dic_copy = dic.copy()
+        for idx in range(start_idx, start_idx + 10):
+            item = discount[idx]
+            if item in dic_copy and dic_copy[item] != 0:
+                dic_copy[item] -= 1
+                
+        # 다 살 수 있는지 확인
+        for k, v in dic_copy.items():
+            if v != 0:
+                break
+        else:
             answer += 1
             
-        start += 1
+        start_idx += 1
         
     return answer
