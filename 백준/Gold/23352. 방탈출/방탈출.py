21d3fcf25f7fec4aeebfda298946f1d4a5bf525f
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-14 22:02:10 +0900

    [Gold V] Title: 방탈출, Time: 4148 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/23352. 방탈출/방탈출.py
+++ 백준/Gold/23352. 방탈출/방탈출.py
@@ -1,52 +1,37 @@
 from collections import deque
 
 n, m = map(int, input().split())
-
 arr = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0] * m for _ in range(n)]
-ans = []
+
+max_len = 0
+candidate = []
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-max_len = 0
-
-def clear_visited():
-    for i in range(n):
-        for j in range(m):
-            visited[i][j] = 0
-
-def bfs(sx, sy):
+def find_short_route(x, y):
     global max_len
-
-    q = deque([(sx, sy)])
-    visited[sx][sy] = 1
+    sx, sy = x, y
+    len_arr = [[int(1e9)] * m for _ in range(n)]
+    q = deque([(x, y)])
+    len_arr[x][y] = 0
 
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-
-            if (0 <= nx < n and 0 <= ny < m and arr[nx][ny] and not visited[nx][ny]):
-                visited[nx][ny] = visited[x][y] + 1
-
-                if (visited[nx][ny] >= max_len):
-                    max_len = visited[nx][ny]
-                    pw = arr[sx][sy] + arr[nx][ny]
-                    ans.append((max_len, pw))
-
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] != 0 and len_arr[nx][ny] > len_arr[x][y] + 1:
+                len_arr[nx][ny] = len_arr[x][y] + 1
                 q.append((nx, ny))
+
+    if len_arr[x][y] >= max_len:
+        max_len = len_arr[x][y]
+        candidate.append((max_len, arr[sx][sy] + arr[x][y]))
 
 for i in range(n):
     for j in range(m):
-        if(arr[i][j]):
-            bfs(i, j)
-            clear_visited()
-
-ans.sort(key=lambda x:(-x[0], -x[1]))
-
-print(ans[0][1])
-
-
-                
+        if arr[i][j] != 0:
+            find_short_route(i, j)
+candidate.sort(key=lambda x: (-x[0], -x[1]))
+print(candidate[0][1])
