d84ca1382b8ef72bc21fe4ba110a3a2dc91de777
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-16 01:02:09 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2700 ms, Memory: 59588 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,15 +1,10 @@
 n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-
-# 누적합 생성
-prefix_sum = [0] * n
-prefix_sum[0] = arr[0]
-for i in range(1, n):
-    prefix_sum[i] = prefix_sum[i - 1] + arr[i]
-
-for _ in range(m):
-    i, j = map(int, input().split())
-    if i == 1:
-        print(prefix_sum[j - 1])
-    else:
-        print(prefix_sum[j - 1] - prefix_sum[i - 2])
+nums = list(map(int, input().split()))
+arr = [list(map(int, input().split())) for _ in range(m)]
+prefix_sum = [0]
+sum = 0
+for i in nums:
+    sum += i
+    prefix_sum.append(sum)
+for i, j in arr:
+    print(prefix_sum[j] - prefix_sum[i - 1])
