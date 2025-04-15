c49c23f2ae8eed1f6c50bf2780f4a3a7d76a56ae
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-15 14:46:10 +0900

    [level 2] Title: 요격 시스템, Time: 377.08 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/README.md
+++ 프로그래머스/2/181188. 요격 시스템/README.md
@@ -1,10 +1,10 @@
 # [level 2] 요격 시스템 - 181188 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188) 
 
 ### 성능 요약
 
-메모리: 91.1 MB, 시간: 315.28 ms
+메모리: 90.2 MB, 시간: 377.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 21일 05:35:43
+2025년 04월 15일 14:46:05
 
 ### 문제 설명
 
--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,12 +1,15 @@
 def solution(targets):
     answer = 1
-    targets.sort(key=lambda x: x[1])
-    s = targets[0][0]
-    e = targets[0][1]
-    
-    for i in range(1, len(targets)):
-        if targets[i][0] >= e:
+    targets.sort(key=lambda x : x[1])
+    end = targets[0][1]
+    targets = targets[1:]
+    for s, e in targets:       
+        if s >= end:           
             answer += 1
-            e = targets[i][1]
+            end = e
+        # print('기준:', end, s, e)
+            
+    # for e in targets:
+    #     print(e)
         
     return answer
