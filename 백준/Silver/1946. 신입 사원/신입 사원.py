14770c789d2e6fac459d66761fafc9deffe35291
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-16 15:55:42 +0900

    [Silver I] Title: 신입 사원, Time: 5156 ms, Memory: 215608 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,22 +1,13 @@
-import sys
-input = sys.stdin.readline
-
 t = int(input())
-
-while t:
+for _ in range(t):
     n = int(input())
-    arr = [tuple(map(int, input().split())) for _ in range(n)]
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    arr.sort() # 서류 기준 정렬
+    top = arr[0][1] # 면접 최고 순위
+    answer = 1 # 1등은 무조건 포함되므로 1로 시작
+    for i in range(1, n):
+        if arr[i][1] < top: # 현재 면접 등수보다 더 높은 등수가 발생하면 최고 면접 등수 갱신
+            top = arr[i][1]
+            answer += 1
 
-    arr.sort()
-
-    ans = 1
-    top = 0
-
-    for i in range(1, len(arr)):
-        if arr[i][1] < arr[top][1]:
-            top = i
-            ans += 1
-            
-    print(ans)
-
-    t -= 1
+    print(answer)
