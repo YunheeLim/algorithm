2f4a44802fcc306ec193fc79324760f8aff6df74
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-18 00:19:25 +0900

    [Gold IV] Title: 알파벳, Time: 5844 ms, Memory: 140268 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,23 +1,29 @@
+import sys
+input = sys.stdin.readline
 
 r, c = map(int, input().split())
-maps = []
-for _ in range(r):
-    maps.append(list(input()))
-ans = 0
-alphas = set()
-dx = [-1, 1, 0, 0]
+arr = [list(input().rstrip()) for _ in range(r)]
+
+answer = 0
+
+dx = [ -1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs(x, y, count):
-    global ans
-    ans = max(ans, count)
+history = set()
+
+def dfs(x, y):
+    global answer
+    answer = max(answer, len(history))
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < r and 0 <= ny < c and not maps[nx][ny] in alphas:
-            alphas.add(maps[nx][ny])
-            dfs(nx, ny, count+1)
-            alphas.remove(maps[nx][ny])
-alphas.add(maps[0][0])
-dfs(0, 0, 1)
-print(ans)
+        if 0 <= nx < r and 0 <= ny < c:
+            if arr[nx][ny] not in history:
+                history.add(arr[nx][ny])
+                dfs(nx, ny)
+                history.remove(arr[nx][ny])
+
+history.add(arr[0][0])
+dfs(0, 0)
+
+print(answer)
