203a15b083aa42013fd3fe6aa60b9925a9d5b548
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-20 00:33:01 +0900

    [Gold V] Title: 퇴사 2, Time: 3172 ms, Memory: 349736 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -2,24 +2,14 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-
-# i일까지의 최대 수익을 저장하는 dp 배열
+arr = [list(map(int, input().split())) for _ in range(n)]
+arr = [0] + arr
 dp = [0] * (n + 1)
 
-schedule = [list(map(int, input().split())) for _ in range(n)]
-
-# 현재까지의 수익
-profit = 0
-
-for i in range(n):
-    # 현재 일수(i)까지의 최대 수익
-    profit = max(profit, dp[i])
-
-    # 상담일자 종료일이 퇴사일 이후인 경우
-    if i + schedule[i][0] > n:
-        continue
-
-    # 상담 종료 일자의 현재값과 갱신할 값 비교 후 큰 값 저장
-    dp[i + schedule[i][0]] = max(profit + schedule[i][1], dp[i + schedule[i][0]])
+for i in range(1, n + 1):
+   dp[i] = max(dp[i], dp[i - 1]) # 현재는 항상 이전일보다 같거나 큼
+   end_date = i + arr[i][0] - 1
+   if end_date <= n:
+     dp[end_date] = max(dp[end_date], dp[i - 1] + arr[i][1])
 
 print(max(dp))
