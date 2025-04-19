57b0eb425c4f767bef80d8fef5e0f8dd698445a2
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-20 01:39:49 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 288 ms, Memory: 50664 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,32 +1,31 @@
 import sys
+sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
-sys.setrecursionlimit(100000)  # 런타임 에러를 피하기 위해 최대 재귀 깊이 재설정
-
 
 v, e = map(int, input().split())
 edges = []
-
 for _ in range(e):
-    a, b, c = map(int, input().split())
-    edges.append((c, a, b))
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+parent = [i for i in range(v + 1)]
+
+edges.sort()
 
 def find_parent(x):
-    if parent[x] != x:
+    if x != parent[x]:
         parent[x] = find_parent(parent[x])
     return parent[x]
 
 def union_parent(a, b):
     a = find_parent(a)
     b = find_parent(b)
-
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
-
+        
 answer = 0
-parent = [i for i in range(v + 1)]
-edges.sort()
 
 for cost, a, b in edges:
     if find_parent(a) != find_parent(b):
