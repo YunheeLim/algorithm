eedabad040503b1f6f15b261d2758a4c77a37cf0
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-20 02:20:53 +0900

    [level 2] Title: 큰 수 만들기, Time: 87.33 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,13 +1,14 @@
 def solution(number, k):
-    answer = ""
+    answer = ''
     stack = []
-    cnt = 0
     for num in number:
         while stack and stack[-1] < num and k > 0:
             stack.pop()
             k -= 1
         stack.append(num)
+        
     if k > 0:
         stack = stack[:-k]
-    answer = ''.join(stack)
+    answer = "".join(stack)
+    
     return answer
