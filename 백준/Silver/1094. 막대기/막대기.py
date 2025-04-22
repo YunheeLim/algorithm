9b408919b218251eb4896e3bf736b7c6778245c0
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-23 01:14:11 +0900

    [Silver V] Title: 막대기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -1,16 +1,13 @@
 x = int(input())
-lines = [64]
-while True:
-    total = sum(lines)
-    if total > x:
-        min_line = min(lines)
-        lines.remove(min_line)
-        half = min_line / 2
-        if total - half >= x:
-            lines.append(half)
-        else:
-            lines.extend([half, half])
-    else:
-        break
 
-print(len(lines))
+n = 64
+answer = 0
+
+while x > 0:
+    if n > x:
+        n //= 2
+    else:
+        x -= n
+        answer += 1
+
+print(answer)
