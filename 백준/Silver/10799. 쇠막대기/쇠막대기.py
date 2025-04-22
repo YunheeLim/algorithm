e7a1fcf470e9f34d5c1ddbdd29b23d0aeeb26238
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-22 20:48:19 +0900

    [Silver II] Title: 쇠막대기, Time: 96 ms, Memory: 112364 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,17 +1,20 @@
-lines = input()
+import sys
+input = sys.stdin.readline
+
+str = input().rstrip()
+
 stack = []
-
-ans = 0
-
-for i in range(len(lines)):
-    if lines[i] == '(':
-        stack.append('(')
+tmp = 0
+answer = 0
+for i in range(len(str)):
+    if str[i] == "(":
+        stack.append(str[i])
     else:
-        if lines[i - 1] == '(':
+        if str[i - 1] == "(": # 레이저
             stack.pop()
-            ans += len(stack)
+            answer += len(stack)
         else:
             stack.pop()
-            ans += 1
-
-print(ans)
+            answer += 1
+     
+print(answer)
