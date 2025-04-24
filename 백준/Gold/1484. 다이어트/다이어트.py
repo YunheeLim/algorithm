32f1a518b8bd55e0c9e2046152083b47acd219a3
Yunhui Lim <lyhhh0320@gmail.com>
2025-04-24 16:52:40 +0900

    [Gold V] Title: 다이어트, Time: 96 ms, Memory: 110328 KB -BaekjoonHub

---
+++ 백준/Gold/1484. 다이어트/다이어트.py
@@ -0,0 +1,17 @@
+G = int(input())
+P = [x for x in range(1, 100001)]
+N, M = 100000, 100000
+left, right = 0, 0 # 투 포인터
+ans = []
+while left < N and right < M:
+    # G는 현재 몸무게(A)의 제곱 - 성원이가 기억하고 있던 몸무게(B)의 제곱
+    # G = A^2 - B^2 => (A + B)*(A - B)
+    tmp = (P[left] + P[right]) * (P[left] - P[right])
+    if tmp == G: ans.append(P[left])
+    if tmp < G:
+        left += 1
+        continue
+    right += 1
+if not ans: print(-1)
+else:
+    for y in ans: print(y)
