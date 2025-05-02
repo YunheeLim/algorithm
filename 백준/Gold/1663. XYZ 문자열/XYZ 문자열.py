d48d81d1ea93be553fcd4ac7e024ed1d2d3d6a04
Yunhui Lim <lyhhh0320@gmail.com>
2025-05-03 02:52:40 +0900

    [Gold I] Title: XYZ 문자열, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/1663. XYZ 문자열/XYZ 문자열.py
+++ 백준/Gold/1663. XYZ 문자열/XYZ 문자열.py
@@ -1,40 +1,37 @@
-import sys
-sys.setrecursionlimit(10000)
-
-t = int(input())
+q = int(input())
 n = int(input())
 
 dp = [0] * 101
-cnt = [[0, 0, 0] for _ in range(101)]
-base = ["", "X", "YZ", "ZX"]
-
 dp[1] = 1
 dp[2] = 2
 dp[3] = 2
+
+cnt = [[0, 0, 0] for _ in range(101)]
 cnt[1] = [1, 0, 0]
 cnt[2] = [0, 1, 1]
 cnt[3] = [1, 0, 1]
 
-# 이분탐색으로 기저문자 찾기
-def find(idx, depth):
-    if depth <= 3:
-        return base[depth][idx - 1]
-    if dp[depth - 3] >= idx: # 왼쪽거
-        return find(idx, depth - 3)
-    else:
-        return find(idx - dp[depth - 3], depth - 2)
-
-
+# 1, 3번
 for i in range(4, n + 1):
     dp[i] = dp[i - 3] + dp[i - 2]
     for j in range(3):
         cnt[i][j] = cnt[i - 3][j] + cnt[i - 2][j]
 
-if t == 1:
+# 2번
+base = ["", "X", "YZ", "ZX"]
+def find(idx, depth):
+    if depth <= 3:
+        return base[depth][idx - 1]
+    if idx <= dp[depth - 3]: # 왼쪽에서 옴
+        return find(idx, depth - 3)
+    else: # 오른쪽에서 옴
+        return find(idx - dp[depth - 3], depth - 2)
+
+if q == 1:
     print(dp[n])
-elif t == 2:
+elif q == 2:
     k = int(input())
     print(find(k, n))
 else:
     ch = input()
-    print(cnt[n][ord(ch) - ord('X')])
+    print(cnt[n][ord(ch)-ord('X')])
